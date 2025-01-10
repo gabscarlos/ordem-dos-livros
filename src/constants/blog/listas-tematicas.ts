d42152b690acc8dafd9f5bc1275a7bb5874856cf
gabscarlos <gabrielcarllos@yahoo.com.br>
2025-01-10 02:29:17 -0300

    lista

--- src/constants/blog/listas-tematicas.ts
+++ src/constants/blog/listas-tematicas.ts
@@ -52,7 +52,63 @@ const listasTematicas = [
   
   Qual desses é o seu favorito?
     `,
-  }
+  },
+
+  {
+    id: "suspense-10-livros",
+    data: "02-01-2025",
+    titulo: "10 Livros de Suspense que Você Precisa Ler",
+    imagem: "https://img.freepik.com/free-photo/woman-with-book-looking-away_23-2147799095.jpg",
+    descricao: "Descubra 10 histórias cheias de mistério, tensão e reviravoltas surpreendentes.",
+    conteudo: `
+  # 10 Livros de Suspense que Você Precisa Ler
+  
+  O suspense é um gênero que prende o leitor desde a primeira página, com histórias recheadas de tensão, enigmas e revelações impactantes. Confira a sinopse de 10 livros imperdíveis:
+  
+  ### 1. *O Silêncio dos Inocentes* - Thomas Harris
+  [![“O Silêncio dos Inocentes” • Divulgação](https://m.media-amazon.com/images/I/91ETm9i7AAL._SY385_.jpg)](https://amzn.to/40pwUzB)
+  A agente do FBI Clarice Starling precisa entrevistar o brilhante, porém perigoso, Dr. Hannibal Lecter para capturar um assassino em série. Um thriller psicológico que explora as profundezas da mente humana.
+  
+  ### 2. *A Garota no Trem* - Paula Hawkins
+  [![“A Garota no Trem” • Divulgação](https://m.media-amazon.com/images/I/A1ajQ9OHOIL._SY385_.jpg)](https://amzn.to/40rJnCH)
+  Rachel observa diariamente um casal aparentemente perfeito do trem. Quando algo estranho acontece, ela se vê envolvida em um mistério repleto de segredos e mentiras.
+  
+  ### 3. *O Código Da Vinci* - Dan Brown
+  [![“O Código Da Vinci” • Divulgação](https://m.media-amazon.com/images/I/710DjekSUkL._SY385_.jpg)](https://amzn.to/42rklFx)
+  O simbologista Robert Langdon investiga um assassinato no Museu do Louvre e descobre pistas escondidas em obras de arte, levando a uma conspiração que pode abalar os fundamentos do cristianismo.
+  
+  ### 4. *Antes de Dormir* - S.J. Watson
+  [![“Antes de Dormir” • Divulgação](https://m.media-amazon.com/images/I/81ZKfVvskUL._SY385_.jpg)](https://amzn.to/4gN4t4o)
+  Christine Lucas perde todas as suas memórias ao dormir. Com ajuda de um diário, ela tenta reconstruir sua vida, mas descobre verdades aterrorizantes sobre si mesma e sua relação com aqueles ao seu redor.
+  
+  ### 5. *A Paciente Silenciosa* - Alex Michaelides
+  [![“A Paciente Silenciosa” • Divulgação](https://m.media-amazon.com/images/I/91R8S52UP6L._SY385_.jpg)](https://amzn.to/425dRM3)
+  Alicia Berenson é acusada de matar o marido e permanece em silêncio desde então. Um psicoterapeuta obcecado pelo caso tenta desvendar seus motivos e acaba envolvido em uma trama surpreendente.
+  
+  ### 6. *Caixa de Pássaros* - Josh Malerman
+  [![“Caixa de Pássaros” • Divulgação](https://m.media-amazon.com/images/I/91263tSVxFL._SY342_.jpg)](https://amzn.to/4j3lDwd)
+  Em um mundo onde abrir os olhos ao ar livre pode levar à morte, Malorie tenta sobreviver com seus filhos. Um thriller pós-apocalíptico repleto de suspense e medo do desconhecido.
+  
+  ### 7. *Objetos Cortantes* - Gillian Flynn
+  [![“Objetos Cortantes” • Divulgação](https://m.media-amazon.com/images/I/518OfKw927L._SY445_SX342_DpWeblab_.jpg)](https://amzn.to/4jlH0ZQ)
+  Camille Preaker, uma repórter, retorna à sua cidade natal para investigar o assassinato de duas garotas. Confrontada com seus próprios traumas, ela descobre segredos obscuros de sua família.
+  
+  ### 8. *O Homem de Giz* - C.J. Tudor
+  [![“O Homem de Giz” • Divulgação](https://m.media-amazon.com/images/I/71oxE2vkZmL._SY385_.jpg)](https://amzn.to/4fS7fUJ)
+  Eddie e seus amigos usavam desenhos de giz para se comunicar. Quando um deles leva ao corpo de uma jovem assassinada, a amizade deles nunca mais será a mesma.
+  
+  ### 9. *Os Homens que Não Amavam as Mulheres* - Stieg Larsson
+  [![“Os Homens que Não Amavam as Mulheres” • Divulgação](https://m.media-amazon.com/images/I/81D8cQc37aS._SY342_.jpg)](https://amzn.to/4h6ZIlZ)
+  O jornalista Mikael Blomkvist e a hacker Lisbeth Salander unem forças para resolver um caso de desaparecimento de décadas, descobrindo uma trama de corrupção e violência.
+  
+  ### 10. *Garota Exemplar* - Gillian Flynn
+  [![“Garota Exemplar” • Divulgação](https://m.media-amazon.com/images/I/41aKH9fwf0L._SY445_SX342_DpWeblab_.jpg)](https://amzn.to/408CHs2)
+  Amy desaparece misteriosamente no dia do aniversário de casamento. Enquanto o marido Nick se torna o principal suspeito, o leitor é levado por uma trama repleta de reviravoltas e manipulações.
+  
+  Qual desses te deixou mais curioso?
+    `,
+  },
+  
   
 ]
 
